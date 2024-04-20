99c1f9d8c673580c57ba0279570cb9468a926b33
Dr Nic Williams <drnicwilliams@gmail.com>
2024-04-20 19:02:32 +1000

    Clean up

--- test/groq/test_client.rb
+++ test/groq/test_client.rb
@@ -3,12 +3,11 @@
 require "test_helper"
 
 class TestGroqClient < Minitest::Test
+  # define "say hello world" for each model, such as: test_hello_world_llama3_8b et al
   Groq::Model::MODELS.each do |model|
-    # define test methods, such as: test_hello_world_llama3_8b et al
     define_method :"test_hello_world_#{model[:model_id]}" do
-      client = Groq::Client.new
-      # vcr cassette
       VCR.use_cassette("#{model[:model_id]}/hello_world") do
+        client = Groq::Client.new
         response = client.post(path: "/openai/v1/chat/completions", body: {
           model: model[:model_id],
           messages: [{role: "user", content: "Reply with only the words: Hello, World!"}]
